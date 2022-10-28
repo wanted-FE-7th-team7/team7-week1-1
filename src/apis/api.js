603f97aef754037051979e362fd58915ef51599f
Han <souq7635@naver.com>
2022-10-28 11:01:43 +0900

    Chore : 환경변수 추가

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const ACCESS_TOKEN = localStorage.getItem('token');
 
 // baseURL: process.env.REACT_BASE_URL,
 export const instance = axios.create({
-  baseURL: `https://pre-onboarding-selection-task.shop`,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
